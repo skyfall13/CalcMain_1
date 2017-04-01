37abfddb48c09d4c6534a6bc56abe54566ab922f
OpalCat <noblesse3539@daum.net>
2017-04-01 21:00:34 +0900

    new project

---
+++ src/main/java/com/cscnu/group13/CalCulate/App.java
@@ -0,0 +1,13 @@
+package com.cscnu.group13.CalCulate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
