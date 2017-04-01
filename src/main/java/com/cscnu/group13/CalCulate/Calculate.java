ea8f96d3479b10e8d02661a5c0953d0c7873422e
OpalCat <noblesse3539@daum.net>
2017-04-01 21:23:18 +0900

    new javafile Calculate.java

---
+++ src/main/java/com/cscnu/group13/CalCulate/Calculate.java
@@ -0,0 +1,23 @@
+package com.cscnu.group13.CalCulate;
+
+class Calculate{
+
+	Object add(Object a, Object b){
+	
+		return (Object) a+b;
+	}
+	
+	Object sub(Object a, Object b){
+		
+		return (Object) a-b;
+	}
+
+	Object mul(Object a, Object b){
+
+		return (Object) a*b;
+	}
+	Object div(Object a, Object b) {
+
+		return (Object) a/b;
+	}
+}
