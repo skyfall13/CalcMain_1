0b79d5a81e73accc2bf8aa39a745c82ad485b428
OpalCat <noblesse3539@daum.net>
2017-04-04 12:39:00 +0900

    메인실행시 인자값을 주면 계산값만 반환하도록 수정

--- team13_calculator/src/main/java/team13/Application.java
+++ team13_calculator/src/main/java/team13/Application.java
@@ -1,24 +1,21 @@
+package team13;
 import java.util.Scanner;
 
 public class Application {
 	
 	private Calculate calculate;
 	
-	public void run(){
+	public void run(String args){
 		
 		this.calculate = new Calculate();
-		Scanner scan = new Scanner(System.in);
-		String input;
+		String input = args;
 		int result;
 		
-		System.out.print("> 수식을 입력하시오: ");
-		while(scan.hasNext()) {
-			input = scan.nextLine();
-			this.calculate.setParse(input);
-			this.calculate.token2Postfix();
-			result = this.calculate.evalPostfix();
-			System.out.println(result);
-			System.out.print("> 수식을 입력하시오: ");
-		}
+		this.calculate.setParse(input);
+		this.calculate.token2Postfix();
+		result = this.calculate.evalPostfix();
+		System.out.println(result);
+		
 	}
+
 }
--- team13_calculator/src/main/java/team13/CalcMain.java
+++ team13_calculator/src/main/java/team13/CalcMain.java
@@ -1,13 +1,11 @@
-
+package team13;
 public class CalcMain {
 
 	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
 		Application app = new Application();
-		app.run();
+		app.run(args[0]);
 		
-//		Parse ps = new Parse("123+17*(3+14)");
-//		System.out.println(ps.getTokens().toString);
 		
 	}
 
